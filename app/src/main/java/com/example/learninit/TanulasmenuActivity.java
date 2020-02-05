65da59c0938754b77301541f5e9c0f947066de42
Jámbor Anna <jambor.anna98@gmail.com>
2020-02-05 20:11:14 +0100

    Style,time lock

--- app/src/main/java/com/example/learninit/TanulasmenuActivity.java
+++ app/src/main/java/com/example/learninit/TanulasmenuActivity.java
@@ -6,10 +6,16 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
+import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.Random;
+import java.util.TimerTask;
 
 public class TanulasmenuActivity extends AppCompatActivity {
 
@@ -17,12 +23,14 @@ public class TanulasmenuActivity extends AppCompatActivity {
     private  Button napibut;
     private  Button tanultBut;
     private SharedPreferences sharedPreferences;
-    //private int randomom;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tanulasmenu);
+
         init();
+
         visszatanulas.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -31,41 +39,59 @@ public class TanulasmenuActivity extends AppCompatActivity {
                 finish();
             }
         });
+
         //itt kezdődik majd a tanulás
+
         napibut.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Random randomom = new Random();
-                int number= randomom.nextInt(3)+1;
-                int szamlalo=0;
-               // sharedPreferences= getSharedPreferences("database",MODE_PRIVATE);
-                for (int i=0; i<1; i++){
-                    if (number==1){
-                        Intent intent = new Intent(TanulasmenuActivity.this,Tanulas1Activity.class);
-                        startActivity(intent);
-                        finish();
-                        szamlalo++;
-                    }
-                    else if (number==2){
-                        Intent intent = new Intent(TanulasmenuActivity.this,Tanulas2Activity.class);
-                        startActivity(intent);
-                        finish();
-                        szamlalo++;
-                    }
-                    else if (number==3){
-                        Intent intent = new Intent(TanulasmenuActivity.this,Tanulas3Activity.class);
-                        startActivity(intent);
-                        finish();
-                        szamlalo++;
-                    }else{
-                        if (szamlalo==10) {
-                            Intent intent = new Intent(TanulasmenuActivity.this, fomenu.class);
+
+                // a napi egyszeri tanulás megengedésére szolgál
+                Calendar jelenlegiDatum = Calendar.getInstance();
+                Calendar tanulasDatuma=Calendar.getInstance();
+                //tanulasDatuma.set(Calendar.YEAR, Calendar.MONTH, Calendar.DAY_OF_MONTH);
+                jelenlegiDatum.set(Calendar.YEAR, Calendar.MONTH, Calendar.DAY_OF_MONTH);
+
+
+                if (jelenlegiDatum.equals(tanulasDatuma)) {
+                    Toast.makeText(TanulasmenuActivity.this, "Térj vissza holnap!", Toast.LENGTH_LONG).show();
+                } else {
+
+
+
+
+                    //Random Activity választó
+                    Random randomom = new Random();
+                    int number = randomom.nextInt(3) + 1;
+
+                    int szamlalo = 0;
+                    // sharedPreferences= getSharedPreferences("database",MODE_PRIVATE);
+                    for (int i = 0; i < 1; i++) {
+                        if (number == 1) {
+                            Intent intent = new Intent(TanulasmenuActivity.this, Tanulas1Activity.class);
                             startActivity(intent);
                             finish();
+                            szamlalo++;
+                        } else if (number == 2) {
+                            Intent intent = new Intent(TanulasmenuActivity.this, Tanulas2Activity.class);
+                            startActivity(intent);
+                            finish();
+                            szamlalo++;
+                        } else if (number == 3) {
+                            Intent intent = new Intent(TanulasmenuActivity.this, Tanulas3Activity.class);
+                            startActivity(intent);
+                            finish();
+                            szamlalo++;
+                        } else {
+                            if (szamlalo == 10) {
+                                Intent intent = new Intent(TanulasmenuActivity.this, fomenu.class);
+                                startActivity(intent);
+                                finish();
+                            }
                         }
+
+
                     }
-
-
                 }
             }
         });

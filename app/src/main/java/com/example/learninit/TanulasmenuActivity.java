e5ce5b034d6d6d7f98fad810a50e4c66bd3cf54d
Jámbor Anna <jambor.anna98@gmail.com>
2020-02-05 20:16:27 +0100

    Style,time lock

--- app/src/main/java/com/example/learninit/TanulasmenuActivity.java
+++ app/src/main/java/com/example/learninit/TanulasmenuActivity.java
@@ -5,17 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
-
-import java.time.format.DateTimeFormatter;
 import java.util.Calendar;
-import java.util.Date;
 import java.util.Random;
-import java.util.TimerTask;
+
 
 public class TanulasmenuActivity extends AppCompatActivity {
 

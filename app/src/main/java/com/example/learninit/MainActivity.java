5f1d456dbf7895833e8a8127e7ad60e430b97980
Jámbor Anna <jambor.anna98@gmail.com>
2020-03-28 23:28:05 +0100

    mentesmentes

--- app/src/main/java/com/example/learninit/MainActivity.java
+++ app/src/main/java/com/example/learninit/MainActivity.java
@@ -1,19 +1,15 @@
 package com.example.learninit;
 
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
-import android.net.wifi.p2p.WifiP2pManager;
-import android.nfc.Tag;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -68,33 +64,6 @@ public class MainActivity extends AppCompatActivity {
         }
 
         }
-   /* public void onBackPressed(){
-        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(MainActivity.this);
-        View alertViev= getLayoutInflater().inflate(R.layout.alert_dialog_style,null);
-        Button igenBut=(Button)alertViev.findViewById(R.id.igenBut);
-        Button nemBut=(Button)alertViev.findViewById(R.id.nemBut);
-
-        alertDialogBuilder.setView(alertViev);
-        final AlertDialog alert = alertDialogBuilder.create();
-        alert.show();
-
-        igenBut.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, Bejelentkezes.class);
-                startActivity(intent);
-                finish();
-            }
-
-        });
-        nemBut.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                alert.dismiss();
-            }
-        });
-
-    }*/
     private  boolean vanWifi(){
         ConnectivityManager connectivityManager= (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo networkInfo= connectivityManager.getActiveNetworkInfo();
@@ -109,20 +78,20 @@ public class MainActivity extends AppCompatActivity {
     }
     private void engedelyek(){
             Log.d(String.valueOf(MainActivity.this), "Engedély kérése");
-            String[] permissions = {Manifest.permission.READ_EXTERNAL_STORAGE,
+            String[] engedely = {Manifest.permission.READ_EXTERNAL_STORAGE,
                     Manifest.permission.WRITE_EXTERNAL_STORAGE};
 
            if(ContextCompat.checkSelfPermission(this.getApplicationContext(),
-                    permissions[0]) == PackageManager.PERMISSION_GRANTED
+                    engedely[0]) == PackageManager.PERMISSION_GRANTED
                     && ContextCompat.checkSelfPermission(this.getApplicationContext(),
-                    permissions[1]) == PackageManager.PERMISSION_GRANTED){
+                    engedely[1]) == PackageManager.PERMISSION_GRANTED){
 
 
 
 
             }else{
                 ActivityCompat.requestPermissions(MainActivity.this,
-                        permissions,
+                        engedely,
                         kerelemKod);
 
             }
